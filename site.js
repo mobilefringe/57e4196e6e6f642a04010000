5a2a73f63ac71732fd4dd770eb467f37a8a3df04
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-22 14:19:54 -0400

    Mira Mesa Market 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "name" : "Mira Mesa Market",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/594c08596e6f646486290300/image/png/1476386371000/default.png",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "http://pico.mallmaverick.com/api/v2/pico/social.json"
+                }
+            
