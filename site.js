8a09d38d692713c3475bdfe5387d2212691810e5
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-06-03 15:28:40 -0400

    Mira Mesa Market 1.3  /site.js

--- site.js
+++ site.js
@@ -2,6 +2,6 @@ var site_json = {
                     "name" : "Mira Mesa Market",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/594c08596e6f646486290300/image/png/1476386371000/default.png",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//pico.mallmaverick.com/api/v4/miramesa/social.json"
+                    "social_feed" : "//miramesa.mallmaverick.com/api/v4/miramesa/social.json"
                 }
             
